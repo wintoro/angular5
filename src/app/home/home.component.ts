3f6b7cd170b67bd48b686b475d378171d9ba6d8e
Wintoro <wintoro@eqlsys.com>
2018-05-23 07:36:56 +0700

    Add animation on add and remove item

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations : [
+
+    trigger('goals', [
+      transition('* => *',[
+        query(':enter', style({ opacity: 0 }), {optional:true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity:0, transform: 'translateY(-75%)', offset:0}),
+            style({opacity:.5, transform: 'translateY(35px)', offset:.3}),
+            style({opacity:1, transform: 'translateY(0)', offset:1}),
+          ]))
+        ]), { optional: true }),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1 }),
+          ]))
+        ]), { optional: true })
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
   itemCount: number = 4;
   btnText: string = 'Add an item';
   goalText: string ='My first life goal';
-  goals = [];
+  goals = ['My first goal', 'I want to climb', 'Go ice skiing'];
 
   constructor() { }
 
@@ -24,4 +49,7 @@ export class HomeComponent implements OnInit {
     this.itemCount = this.goals.length;
   }
 
+  removeItem(i){
+    this.goals.splice(i, 1);
+  }
 }
